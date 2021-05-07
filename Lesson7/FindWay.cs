8aba5abd6b6d5e04dcd0c2458cafbfd6fd1f8b05
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-05-07 15:37:55 +0300

    final

---
+++ Lesson7/FindWay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson7
+{
+    public static class FindWay
+    {
+        public static int WaysNumber(int a, int b)
+        {
+            if (a <= 0) throw new ArgumentOutOfRangeException(nameof(a));
+            if (b <= 0) throw new ArgumentOutOfRangeException(nameof(b));
+
+            if (a == 1 || b == 1)
+            {
+                return 1;
+            }
+
+            return WaysNumber(a, b - 1) + WaysNumber(a - 1, b);
+        }
+    }
+}
--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -1,11 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Lesson7
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        [Theory]
+        [MemberData(nameof(Test_Data_FindWay))]
+        public void Test_FindWay(int a, int b, int expectedWaysNum, bool IsException)
         {
+            //Arrange
+
+            // 1  1  1  1
+            // 1  2  2  4
+            // 1  2  6 10
+            // 1  4 10 20
+
+            if (IsException)
+            {
+                //Act - Assert
+                Assert.Throws<ArgumentOutOfRangeException>(() => FindWay.WaysNumber(a, b));
+            }
+            else
+            {
+                //Act
+                var ways = FindWay.WaysNumber(a, b);
+
+                //Assert
+                Assert.Equal(expectedWaysNum, ways);
+            }
+            
+        }
+
+        public static IEnumerable<object[]> Test_Data_FindWay()
+        {
+            yield return new object[] { -1, -2, 1, true };
+            yield return new object[] { 0, 0, 1, true };
+
+            yield return new object[] { 1, 1, 1, false };
+            yield return new object[] { 2, 2, 2, false };
+            yield return new object[] { 2, 4, 4, false };
+            yield return new object[] { 3, 3, 6, false };
+            yield return new object[] { 3, 4, 10, false };
+            yield return new object[] { 4, 3, 10, false };
+            yield return new object[] { 4, 4, 20, false };
         }
     }
 }
