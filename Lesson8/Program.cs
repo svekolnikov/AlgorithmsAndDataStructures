404a111c788372501840c997f837779e457c408d
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-05-07 21:18:48 +0300

    +

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -6,6 +6,13 @@ namespace Lesson8
     {
         static void Main(string[] args)
         {
+            var arrNotSort = new int[] {98, 92, 87, 85, 77, 71, 69, 63, 55, 56, 42, 43, 38, 30, 20, 21, 13, 17, 4, 7};
+            var result = Sort.MergeSort(arrNotSort);
+            foreach (var i in result)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Lesson8/Sort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson8
+{
+    public static class Sort
+    {
+        public static int[] BucketSort(int[] arr)
+        {
+            //Divide
+
+            //BucketSort
+
+            //Merge
+
+
+            return arr;
+        }
+
+
+        public static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+
+                for (int i = left; i <= middleIndex; i++)
+                {
+                    tempArray[index] = array[i];
+                    index++;
+                }
+
+                for (int i = right; i <= highIndex; i++)
+                {
+                    tempArray[index] = array[i];
+                    index++;
+                }
+
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    array[lowIndex + i] = tempArray[i];
+                }
+            }
+        }
+
+        public static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                if (highIndex - lowIndex == 1)
+                {
+                    if (array[highIndex] < array[lowIndex])
+                    {
+                        var t = array[lowIndex];
+                        array[lowIndex] = array[highIndex];
+                        array[highIndex] = t;
+                    }
+                }
+                else
+                {
+                    var middleIndex = (lowIndex + highIndex) / 2;
+                    MergeSort(array, lowIndex, middleIndex);
+                    MergeSort(array, middleIndex + 1, highIndex);
+                    Merge(array, lowIndex, middleIndex, highIndex);
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+    }
+}
